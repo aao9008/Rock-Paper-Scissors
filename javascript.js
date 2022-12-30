1e921e20776735011b8595e5d4f43111a7e45cb6
Alfredo Ormeno <ormenoandres13@gmail.com>
2022-12-29 21:11:51 -0500

    Add getPlayerChoice and playRound functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+// Ask player for their selection and format selection 
+let player_selection = prompt("Rock, paper, or scissors?");
+player_selection = player_selection.toLowerCase();
+let validChoice = false;
+
+if (player_selection !== "rock" || player_selection != "paper" || player_selection != "scissors")
+{
+    while (validChoice === false)
+    {    
+        alert("Please pick 'rock', 'paper', or 'scissors'!");
+        player_selection = prompt("Rock, paper, or scissors?").toLowerCase;
+        
+        if (player_selection !== "rock" || player_selection != "paper" || player_selection != "scissors")
+        {
+            validChoice = true;
+        }
+    }
+}
+
 function getComputerChoice(){
     // Create list of options
     let choices = ["rock","paper","scissors"];
@@ -8,4 +27,12 @@ function getComputerChoice(){
     // Pick a choice using a random number and return choice
     let choice = choices[result];
     return choice;
+}
+
+function playRound(playerSelection, computerSelection){
+    
+
+    // Compare player selection to computer selection
+
+    //Declare winner of round 
 }
